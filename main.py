bc0f2f356a4e256c56c58249820cac51de8f738e
offensive-io <greenscrean401@gmail.com>
2025-10-28 19:17:31 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from logger import log_location
+from map_viewer import show_map
 
+def main():
+    print("Welcome to Ubique!")
+    log_location()
+    show_map()
+
+if __name__ == "__main__":
+    main()
